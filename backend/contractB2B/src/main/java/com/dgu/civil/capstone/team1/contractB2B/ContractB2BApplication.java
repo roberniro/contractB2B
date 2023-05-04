df4f7d66807759386e559d5d98f9c4dce353c3be
Hankyul Min <denirokr22@gmail.com>
2023-05-04 17:23:11 +0900

    [23.05.04 Hankyul] (add) 스프링부트, 리액트 프로젝트 생성

---
+++ backend/contractB2B/src/main/java/com/dgu/civil/capstone/team1/contractB2B/ContractB2BApplication.java
@@ -0,0 +1,13 @@
+package com.dgu.civil.capstone.team1.contractB2B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractB2BApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContractB2BApplication.class, args);
+	}
+
+}
---
+++ backend/contractB2B/src/test/java/com/dgu/civil/capstone/team1/contractB2B/ContractB2BApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgu.civil.capstone.team1.contractB2B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContractB2BApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
