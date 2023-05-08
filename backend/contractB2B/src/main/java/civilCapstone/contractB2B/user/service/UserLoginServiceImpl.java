cdfe63ba7402d66659e82dd03ed81ef461ab6750
Hankyul Min <denirokr22@gmail.com>
2023-05-08 14:16:09 +0900

    [2023.05.08 Hankyul] (Add) UserLoginServiceImpl

---
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/user/service/UserLoginServiceImpl.java
@@ -0,0 +1,70 @@
+package civilCapstone.contractB2B.user.service;
+
+import civilCapstone.contractB2B.global.model.ResponseDto;
+import civilCapstone.contractB2B.user.model.UserDto;
+import civilCapstone.contractB2B.user.entity.User;
+import civilCapstone.contractB2B.user.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Service
+public class UserLoginServiceImpl implements UserLoginService{
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private TokenProvider tokenProvider;
+    // 비밀번호 암호화위한 encoder
+    private PasswordEncoder encoder = new BCryptPasswordEncoder();
+
+    public ResponseEntity<?> getResponseEntity(UserDto userDto) {
+        User user = authenticateUsername(userDto.getUsername()); // username으로 사용자 찾기
+        if (user == null) { // 유저가 없으면 에러
+            String message = " 올바르지 않습니다.";
+            ResponseDto responseErrorDto = getResponseErrorDto(message);
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
+        if (!encoder.matches(userDto.getPassword(), user.getPassword())) { // 유저가 있으면 비밀번호가 맞는지 확인, 틀리면 에러
+            String message = "비밀번호가 올바르지 않습니다.";
+            ResponseDto responseErrorDto = getResponseErrorDto(message);
+            return ResponseEntity.badRequest().body(responseErrorDto);
+        }
+        final String token = tokenProvider.create(user); // 토큰 생성
+        final UserDto responseUserDto = getResponseUserDto(userDto, user, token); // 유저 정보를 담은 dto 생성
+        return ResponseEntity.ok().body(responseUserDto); // dto 반환
+    }
+
+
+    private static ResponseDto getResponseErrorDto(String message) { // 에러 메시지를 담은 dto 생성
+        Map<String, String> loginResult = new HashMap<>();
+        loginResult.put("valid_login", message);
+        ResponseDto responseDto = ResponseDto.builder().error(loginResult).build();
+        return responseDto;
+    }
+
+    private static UserDto getResponseUserDto(UserDto userDto, User user, String token) { // 유저 정보를 담은 dto 생성
+        final UserDto responseUserDto = userDto.builder()
+                .id(user.getId())
+                .username(user.getUsername())
+                .token(token)
+                .companyName(user.getCompanyName())
+                .nip(user.getNip())
+                .address(user.getAddress())
+                .contact(user.getContact())
+                .role(user.getRole())
+                .build();
+        return responseUserDto;
+    }
+
+    public User authenticateUsername(final String username) { // username으로 유저 찾기
+        return userRepository.findByUsername(username)
+                .orElse(null);
+    }
+}
