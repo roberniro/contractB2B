f4a1518b517ffabbca43070d451287be41c3fadd
Hankyul Min <denirokr22@gmail.com>
2023-06-01 20:02:54 +0900

    [2023.06.01 Hankyul] (Mod) ContractorModal 스타일

--- frontend/contractb2b/src/components/Modal/Client/ContractorModal.js
+++ frontend/contractb2b/src/components/Modal/Client/ContractorModal.js
@@ -3,7 +3,7 @@ import { Modal, Button, Table } from "react-bootstrap";
 
 const ContractorModal = ({ company, showModal, handleCloseModal }) => {
   return (
-    <Modal show={showModal} onHide={handleCloseModal} size="lg">
+    <Modal show={showModal} onHide={handleCloseModal} size="xl">
       <Modal.Header closeButton>
         <Modal.Title>업체 정보</Modal.Title>
       </Modal.Header>
@@ -20,18 +20,18 @@ const ContractorModal = ({ company, showModal, handleCloseModal }) => {
             <strong>연락처:</strong> {company.contact}
           </li>
           <li>
-            <strong>평점:</strong> {company.rating}
+            <strong>평점:</strong> {Number(company.rating).toFixed(1)}
           </li>
           <li>
             <strong>경력:</strong>
             <Table className="mb-0">
               <colgroup>
-                <col style={{ width: "10%" }} />
-                <col style={{ width: "10%" }} />
+                <col style={{ width: "15%" }} />
+                <col style={{ width: "7%" }} />
                 <col style={{ width: "10%" }} />
                 <col style={{ width: "20%" }} />
-                <col style={{ width: "20%" }} />
-                <col style={{ width: "30%" }} />
+                <col style={{ width: "18%" }} />
+                <col style={{ width: "25%" }} />
               </colgroup>
               <thead>
                 <tr>
@@ -46,12 +46,24 @@ const ContractorModal = ({ company, showModal, handleCloseModal }) => {
               <tbody>
                 {company.experienceDtoList.map((experience, index) => (
                   <tr key={index}>
-                    <td>{experience.name}</td>
-                    <td>{experience.field}</td>
-                    <td>{experience.budget}</td>
-                    <td>{experience.site}</td>
-                    <td>{experience.period}</td>
-                    <td>{experience.content}</td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.name}
+                    </td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.field}
+                    </td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.budget}
+                    </td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.site}
+                    </td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.period}
+                    </td>
+                    <td style={{ verticalAlign: "middle", fontSize: "80%" }}>
+                      {experience.content}
+                    </td>
                   </tr>
                 ))}
               </tbody>
