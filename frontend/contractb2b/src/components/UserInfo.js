fbbe28220bb38ab33ec472715d731e16b3c3af4b
Hankyul Min <denirokr22@gmail.com>
2023-06-01 13:56:08 +0900

    [2023.06.01 Hankyul] (Mod) UserInfo.js 스타일

--- frontend/contractb2b/src/components/UserInfo.js
+++ frontend/contractb2b/src/components/UserInfo.js
@@ -4,7 +4,7 @@ const UserInfo = () => {
   const name = sessionStorage.getItem("name");
   const role = sessionStorage.getItem("role");
 
-  return <span className="mr-2">{`${name}(${role})님 환영합니다.`}</span>;
+  return <span className="text-white">{`${name}(${role})님 환영합니다.`}</span>;
 };
 
 export default UserInfo;
