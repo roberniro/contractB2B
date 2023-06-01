492ac647d07882041de1b76877c63dfb41f0bf1d
Hankyul Min <denirokr22@gmail.com>
2023-06-01 13:52:11 +0900

    [2023.06.01 Hankyul] (Fix) 견적 수락, 삭제시 발생한 오류 수정(String 비교, 주소 객체 생성)

--- backend/contractB2B/src/main/java/civilCapstone/contractB2B/client/service/ClientEstimateStatusService.java
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/client/service/ClientEstimateStatusService.java
@@ -1,6 +1,8 @@
 package civilCapstone.contractB2B.client.service;
 
+import civilCapstone.contractB2B.global.entity.Address;
 import civilCapstone.contractB2B.global.entity.Construction;
+import civilCapstone.contractB2B.global.entity.ConstructionStatus;
 import civilCapstone.contractB2B.global.entity.Estimate;
 import civilCapstone.contractB2B.global.model.EstimateDto;
 import civilCapstone.contractB2B.global.model.ResponseDto;
@@ -27,17 +29,17 @@ public class ClientEstimateStatusService {
     @Autowired
     private ConstructionRepository constructionRepository;
 
-    public ResponseEntity acceptEstimate(String username, Long estimateId, EstimateDto.EstimateRequestDto estimateRequestDto) {
+    public ResponseEntity acceptEstimate(String username, String estimateId, EstimateDto.EstimateRequestDto estimateRequestDto) {
         if (userRepository.findByUsername(username).get().getRole() != Role.CLIENT) {
             ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "사용자가 클라이언트가 아닙니다")).build();
             return ResponseEntity.badRequest().body(responseErrorDto);
         }
-        if (!estimateRepository.existsById(estimateId)) {
+        if (!estimateRepository.existsById(Long.parseLong(estimateId))) {
             ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서가 존재하지 않습니다")).build();
             return ResponseEntity.badRequest().body(responseErrorDto);
         }
-        Estimate estimate = estimateRepository.findById(estimateId).get();
-        if (estimate.getClient().getUsername() != username) {
+        Estimate estimate = estimateRepository.findById(Long.parseLong(estimateId)).get();
+        if (!estimate.getClient().getUsername().equals(username)) {
             ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서의 클라이언트가 아닙니다")).build();
             return ResponseEntity.badRequest().body(responseErrorDto);
         }
@@ -49,22 +51,28 @@ public class ClientEstimateStatusService {
         return ResponseEntity.ok().body(estimateStatusResponseDto);
     }
 
-    public ResponseEntity deleteEstimate(String username, Long estimateId) {
-        if (userRepository.findByUsername(username).get().getRole() != Role.CLIENT) {
-            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "사용자가 클라이언트가 아닙니다")).build();
-            return ResponseEntity.badRequest().body(responseErrorDto);
+    public ResponseEntity deleteEstimate(String username, String estimateId) {
+        try {
+            if (userRepository.findByUsername(username).get().getRole() != Role.CLIENT) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "사용자가 클라이언트가 아닙니다")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
+            }
+            if (!estimateRepository.existsById(Long.parseLong(estimateId))) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서가 존재하지 않습니다")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
+            }
+            Estimate estimate = estimateRepository.findById(Long.parseLong(estimateId)).get();
+            if (!estimate.getClient().getUsername().equals(username)) {
+                ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서의 클라이언트가 아닙니다")).build();
+                return ResponseEntity.badRequest().body(responseErrorDto);
+            }
+            estimateRepository.delete(estimate);
+            return ResponseEntity.ok().body(Collections.singletonMap("delete_estimate", "견적서가 삭제되었습니다"));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return ResponseEntity.badRequest().body(Collections.singletonMap("delete_estimate", "견적서 삭제에 실패했습니다"));
         }
-        if (!estimateRepository.existsById(estimateId)) {
-            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서가 존재하지 않습니다")).build();
-            return ResponseEntity.badRequest().body(responseErrorDto);
-        }
-        Estimate estimate = estimateRepository.findById(estimateId).get();
-        if (estimate.getClient().getUsername() != username) {
-            ResponseDto responseErrorDto = ResponseDto.builder().error(Collections.singletonMap("create_estimate", "견적서의 클라이언트가 아닙니다")).build();
-            return ResponseEntity.badRequest().body(responseErrorDto);
-        }
-        estimateRepository.deleteById(estimateId);
-        return ResponseEntity.ok().body(Collections.singletonMap("delete_estimate", "견적서가 삭제되었습니다"));
+
     }
 
     private EstimateDto getEstimateStatusDto(Estimate estimate) {
@@ -88,15 +96,22 @@ public class ClientEstimateStatusService {
     }
 
     private Construction estimateToConstruction(Estimate estimate, EstimateDto.EstimateRequestDto estimateRequestDto) {
+        Address constructionAddress = Address.builder().
+                city(estimate.getSite().getCity())
+                .district(estimate.getSite().getDistrict())
+                .addressDetail(estimate.getSite().getAddressDetail())
+                .build();
         Construction construction = Construction.builder()
+                .name(estimate.getName())
                 .client(estimate.getClient())
                 .contractor(estimate.getContractor())
                 .field(estimate.getField())
-                .site(estimate.getSite())
+                .site(constructionAddress)
                 .period(estimate.getPeriod())
                 .budget(estimate.getBudget())
                 .contractContent(estimate.getClientContent())
                 .reason(estimateRequestDto.getReason())
+                .constructionStatus(ConstructionStatus.WAITING)
                 .build();
         return construction;
     }
