1fc105699de5043bb8bf32d2ce9df8ed2be3ae9a
Hankyul Min <denirokr22@gmail.com>
2023-06-01 13:53:27 +0900

    [2023.06.01 Hankyul] (Mod) WebConfig cors allowed method

--- backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/service/WebConfig.java
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/service/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000")
-                .allowedMethods("*");
+                .allowedMethods("PUT", "DELETE", "GET", "POST", "PATCH");
     }
 }
