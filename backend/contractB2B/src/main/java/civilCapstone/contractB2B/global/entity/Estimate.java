bb70d5a55dfc3f9da2f96bf6e4f3b2caeb42d26d
Hankyul Min <denirokr22@gmail.com>
2023-06-01 13:48:28 +0900

    [2023.06.01 Hankyul] (Fix) Estimate 의존관계, ColumnDefault 에러 수정

--- backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/entity/Estimate.java
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/entity/Estimate.java
@@ -14,36 +14,36 @@ import javax.persistence.*;
 @Builder
 @Entity
 @Getter
-@Table
+@Table(name = "estimate")
 public class Estimate {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false, nullable = false)
     private Long id;
-    @Column(length = 20, nullable = false)
+    @Column(nullable = false)
     private String name;
-    @Column(length = 10, nullable = true)
+    @Column(nullable = true)
     private String motherId;
 
     @JoinColumn(name = "client_id", nullable = false)
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User client;
 
     @JoinColumn(name = "contractor_id", nullable = false)
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     private User contractor;
 
-    @Column(length = 20, nullable = false)
+    @Column(nullable = false)
     private String field;
 
     @JoinColumn(name = "site_id", nullable = false)
-    @OneToOne(cascade = CascadeType.ALL)
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Address site;
 
-    @Column(length = 20, nullable = false)
+    @Column(nullable = false)
     private String period;
 
-    @Column(length = 20, nullable = false)
+    @Column(nullable = false)
     private String budget;
 
     @Column(name = "client_content", length = 200, nullable = false)
@@ -53,7 +53,6 @@ public class Estimate {
     private String contractorContent;
 
     @Column(name="status", nullable = false)
-    @ColumnDefault("PENDING")
     private EstimateStatus estimateStatus;
 
     public void setSite(Address address) {
