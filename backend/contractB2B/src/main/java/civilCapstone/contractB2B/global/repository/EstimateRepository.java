8a6edb59f40072a05133c245ceba8d9d9b2e107f
Hankyul Min <denirokr22@gmail.com>
2023-06-01 19:59:27 +0900

    [2023.06.01 Hankyul] (Mod) EstimateRepository 메소드 수정

--- backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/repository/EstimateRepository.java
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/repository/EstimateRepository.java
@@ -9,15 +9,8 @@ import java.util.List;
 
 @Repository
 public interface EstimateRepository extends JpaRepository<Estimate, Long> {
-    Estimate findByContractorId(Long contractorId);
-    Estimate findByClientId(Long clientId);
-    Estimate findByContractorIdAndClientId(Long contractorId, Long clientId);
-    List<Estimate> findAllByClient_Id(Long clientId);
-    List<Estimate> findAllByMotherId(String motherId);
-    boolean existsByClient_Id(Long id);
     boolean existsByMotherId(String motherId);
-
-    List<Estimate> findAllByContractor_Id(Long contractorId);
-
     List<Estimate> findAllByContractor(User contractor);
+    boolean existsByClient(User user);
+    boolean existsByContractor(User user);
 }
