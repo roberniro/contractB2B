9a989bb3687e5a5c744b8339875adfc672b7ee4b
Hankyul Min <denirokr22@gmail.com>
2023-06-01 19:53:42 +0900

    [2023.06.01 Hankyul] (Mod) Validator 별도 클래스로 분리

---
+++ backend/contractB2B/src/main/java/civilCapstone/contractB2B/global/service/Validator.java
@@ -0,0 +1,24 @@
+package civilCapstone.contractB2B.global.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class Validator {
+    @Transactional(readOnly = true)
+    public Map<String, String> validateHandling(Errors errors) {
+        Map<String, String> validatorResult = new HashMap<>();
+        for (FieldError error : errors.getFieldErrors()) {
+            String validKeyName = String.format("valid_%s", error.getField());
+            validatorResult.put(validKeyName, error.getDefaultMessage());
+        }
+        return validatorResult;
+    }
+}
