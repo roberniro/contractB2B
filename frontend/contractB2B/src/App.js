216a215923e9370e9a8cfe504c0a6073fffa03fa
Hankyul Min <denirokr22@gmail.com>
2023-06-20 19:03:21 +0900

    [2023.06.20 Hankyul] (Add) 라우터

--- frontend/contractB2B/src/App.js
+++ frontend/contractB2B/src/App.js
@@ -1,8 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Login from "./pages/Login";
 import Join from "./pages/Join";
 import Client from "./pages/Client";
+import Contractor from "./pages/Contractor";
+import Citizen from "./pages/Citizen";
 
+// 라우팅 컴포넌트
 function App() {
   return (
     <div className="root">
@@ -11,6 +15,8 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/join" element={<Join />} />
         <Route path="/client" element={<Client />} />
+        <Route path="/contractor" element={<Contractor />} />
+        <Route path="/citizen" element={<Citizen />} />
       </Routes>
     </div>
   );
